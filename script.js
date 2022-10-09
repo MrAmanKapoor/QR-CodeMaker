2821815e0bfcdbede774549c0c323b4889e33107
Aman Kapoor <amankapoor140@gmail.com>
2022-10-09 14:06:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,31 +32,3 @@ qrInput.addEventListener("keyup", () => {
     }
 });
 
-
-/*
-
-
-const card = document.querySelector(".card"),
-    qrInput = card.querySelector(".input-field textarea"),
-    qrImg = card.querySelector(".qr-code img");
-   
-
-    
-
-function changeinfo() {
-    
-
-    if (!qrInput.value) return;
-    document.getElementById("character").innerHTML=qrInput.value.length +(" Characters Or ");
-    document.getElementById("words").innerHTML= qrInput.value.split(" ").length +(" Words");
-    let url1 = "https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=";
-    let url2 = qrInput.value;
-    let result = url1.concat(url2);
-    qrImg.src = result;
-    document.getElementById("download").href = result;
-
-    qrImg.addEventListener("load", () =>
-        card.classList.add("active"),
-    )
-}
-*/
